711e0d1f5f7c192feafae2f603922fc60db79de4
emmanuelishola123 <emmanuelishola7@gmail.com>
2022-12-21 00:55:08 +0100

    update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from utils.cardbacks import scrape as cardbacks_scrapper
 # CONFIG FOR PRODUCTION ENV
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -18,4 +18,3 @@ chrome_options.add_experimental_option('useAutomationExtension', False)
 
 print('Scraping in progress...........................', now())
 cardbacks_scrapper(webdriver, Service, chrome_options, WebDriverWait, By, EC)
-
